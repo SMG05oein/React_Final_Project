fda97f359790f2aa65a2ce1d9c0807b2f70dd438
SMG <SMG05eoin@gmail.com>
2025-05-17 19:48:58 +0900

    UI개선

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,40 +3,37 @@ import Banner from "./components/banner/Banner";
 import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 import {usePopularMoviesQuery} from "../../hooks/usePopularMovies";
 import {Alert} from "react-bootstrap";
+import './HomePage.style.css'
 
 const HomePage = () => {
     const API = usePopularMoviesQuery();
-    console.log("HomePage: " ,API);
-    try{
-        if(API.isLoading){
-            return <h1>Loading...</h1>
-        }
-        return (
-            <div>
-                <Banner API={API}/>
-                <PopularMovieSlide API={API}/>
-            </div>
-        );
+    console.log("HomePage: ", API);
 
-    }catch{
-        return(
-            <div>
-                {/*에러 메시지:  {API[0]?.error.message}*/}
-                <h1>에러 발생...</h1>
-                {(!API.isError && API.error[0] == null) ? (<div>에러 원인 찾는 중...</div>) : (<div>에러 메시지: {API.error[0]?.message}</div>)}
-            </div>
-        )
+    if (API.isLoading) {
+        return <h1 className={"loading"}></h1>;
     }
 
+    if (API.isError) {
+        return (
+            <div>
+                <h1>에러 발생...</h1>
+                {API.error? (
+                    <Alert variant={"danger"}>에러 메시지: {API.error[0]?.message}</Alert>
+                ) : (
+                    <div>에러 원인 찾는 중...</div>
+                )}
+            </div>
+        );
+    }
 
-
-        // return (
-        //     <div>
-        //         <Banner/>
-        //         <PopularMovieSlide/>
-        //     </div>
-        // );
-
+    return (
+        <div>
+            <Banner API={API} />
+            <PopularMovieSlide API={API} />
+        </div>
+    );
 };
+
+
 
 export default HomePage;
