d15ccb0aedd380e4ffb3e83aa9d23c0cf77e1d21
SMG <SMG05eoin@gmail.com>
2025-05-18 12:47:43 +0900

    리뷰 UI개선 1

--- src/pages/MoviesView/Review/Review.jsx
+++ src/pages/MoviesView/Review/Review.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useReview} from "../../../hooks/useReview";
 import {Col, Container, Row} from "react-bootstrap";
+import './Review.style.css'
 
 const Review = ({idx}) => {
     const {data, isLoading, isError, error} = useReview(idx);
@@ -10,18 +11,20 @@ const Review = ({idx}) => {
         <div>
             <h3>리뷰: {data?.length}개</h3>
             <hr style={{marginTop: '2px'}}/>
-            <Container>
                 {data?.map((item) => (
-                    <Row>
-                        <Col><strong>작성자: {item.author}</strong></Col>
-                        <Col>작성일: {item.created_at.substr(0, 10)}</Col>
-                        <Row>
-                            <Col>{item.content}</Col>
-                        </Row>
-                        <hr/>
-                    </Row>
+                    <Container>
+                        <div className={"Box"}>
+                            <Row>
+                                <Col><strong>작성자: {item.author}</strong></Col>
+                                <Col><div>작성일: {item.created_at.substr(0, 10)}</div></Col>
+                                <Row>
+                                    <Col>{item.content}</Col>
+                                </Row>
+                                {/*<hr/>*/}
+                            </Row>
+                        </div>
+                    </Container>
                 ))}
-            </Container>
         </div>
     );
 };
