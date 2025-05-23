65d50e60e7eaf72756374d9a5d19bba2b37ba7de
SMG <SMG05eoin@gmail.com>
2025-05-23 20:17:21 +0900

    필터 리펙토링

--- src/pages/Movies/filters/GenreFilter.jsx
+++ src/pages/Movies/filters/GenreFilter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./GenreFilter.style.css"
 import {useMoviesQuery} from "../../../hooks/usseMoviesGenre";
+import {FormCheck} from "react-bootstrap";
 
 const GenreFilter = ({setG}) => {
     const {data} = useMoviesQuery();
@@ -16,10 +17,12 @@ const GenreFilter = ({setG}) => {
         <div className="GenreFilter">
             {data?.map((genre) => (
                 <div>
-                    <input type={"checkbox"} value={genre.name} style={{marginBottom:'10px', marginRight: '5px'}}
+                    <FormCheck style={{marginBottom:'5px'}} type="checkbox" value={genre.name} label={genre.name}
                     onChange={(event) => handleCheckboxChange(event, genre.id)}/>
-                    {genre.name}
-                    <br/>
+                    {/*<input type={"checkbox"} value={genre.name} style={{marginBottom:'10px', marginRight: '5px'}}*/}
+                    {/*onChange={(event) => handleCheckboxChange(event, genre.id)}/>*/}
+                    {/*{genre.name}*/}
+                    {/*<br/>*/}
                 </div>
             ))}
         </div>
